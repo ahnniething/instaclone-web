447fb3dbe4ef90317386364b67589a6668136b5c
ahnniething <ahnniething@gmail.com>
2022-11-19 00:02:15 +0900

    style: make dark mode options using reactive variables and theme in styled-components

--- src/apollo.js
+++ src/apollo.js
@@ -1,14 +1,23 @@
 /* Reactive Variable 
+  Apollo Client 3의 새로운 기능, reactive variables는 
+  Apollo Client 캐시 외부의 local state를 나타내는 유용한 메커니즘입니다. 
+  캐시와 분리되어 있기 때문에 reactive variables는 모든 유형과 구조의 데이터를 저장할 수 있으며
+  GraphQL 구문을 사용하지 않고도 애플리케이션의 어느 곳에서나 상호 작용할 수 있습니다. 
+  가장 중요한 것은 reactive variable를 수정하면 
+  해당 변수에 의존하는 모든 활성 쿼리의 업데이트가 트리거된다는 것입니다.
+
 / what could be a reactive variable? 
  1. isLoggedIn
  2. isDarkMode
  values that don't want to be passing down props and props and props...
- "reactive variables can store data of any type and structure, 
-  and you can interact with them anywhere in your application 
-  without using GraphQL syntax."
+
+  useReactiveVar
+  useReactiveVar()훅을 사용하여 reactive variable를 직접 읽고 반응할 수 있습니다.
+
   여기에 reative variable 을 선언한 뒤
-  실제로 컴포넌트에서 사용할 때는 useReactiveVar() Hook을 사용한다.
+  실제로 컴포넌트에서 사용할 때는 useReactiveVar() Hook을 사용하여 접근한다.
 */
 import { makeVar } from "@apollo/client";
 
 export const isLoggedInVar = makeVar(false);
+export const darkModeVar = makeVar(false);
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,26 +1,12 @@
 import styled, { css } from "styled-components";
-import { isLoggedInVar } from "../apollo";
+import { darkModeVar, isLoggedInVar } from "../apollo";
 import { useState } from "react";
 const Title = styled.h1`
-  color: ${(props) => (props.potato ? "beige" : "violet")};
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  ${(props) =>
-    props.potato
-      ? css`
-          font-size: 49px;
-        `
-      : css`
-          text-decoration: underline;
-        `}
+  color: ${(props) => props.theme.fontColor};
 `;
 
 const Container = styled.div`
-  background-color: tomato;
-`;
-
-const TogglePotato = styled.button`
-  color: red;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 function Login() {
@@ -28,9 +14,10 @@ function Login() {
   const togglePotato = () => setPotato((current) => !current);
   return (
     <Container>
-      <Title potato={potato}>Login</Title>
-      <TogglePotato onClick={togglePotato}>Toggle Potato</TogglePotato>
+      <Title>Login</Title>
       <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
+      <button onClick={() => darkModeVar(true)}>Dark Mode</button>
+      <button onClick={() => darkModeVar(false)}>light Mode</button>
     </Container>
   );
 }
