fd11ff2e64432240aa27ee37944f39ae8bde4dd3
ahnniething <ahnniething@gmail.com>
2022-11-18 23:10:59 +0900

    style: create component by sending props to styled-components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,29 @@
+import styled from "styled-components";
 import { isLoggedInVar } from "../apollo";
+import { useState } from "react";
+const Title = styled.h1`
+  color: ${(props) => (props.potato ? "beige" : "violet")};
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
 
-function Login({ setIsLoggedIn }) {
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+  color: red;
+`;
+
+function Login() {
+  const [potato, setPotato] = useState(false);
+  const togglePotato = () => setPotato((current) => !current);
   return (
-    <div>
-      <h1>Login</h1>
+    <Container>
+      <Title potato={potato}>Login</Title>
+      <TogglePotato onClick={togglePotato}>Toggle Potato</TogglePotato>
       <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
-    </div>
+    </Container>
   );
 }
 export default Login;
