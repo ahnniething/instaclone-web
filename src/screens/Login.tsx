fcd14f59bc64cc10a37a2b1de291548c72ee7852
ahnniething <ahnniething@gmail.com>
2022-11-19 23:45:07 +0900

    feat: modify a form using react-hook-form

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -13,6 +13,7 @@ import FormBox from "../components/auth/FormBox";
 import BottomBox from "../components/auth/BottomBox";
 import { useState } from "react";
 import PageTitle from "../components/PageTitle";
+import { useForm } from "react-hook-form";
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -23,23 +24,23 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [usernameError, setUsernameError] = useState("");
-  const onUsernameChange = (event: any) => {
-    console.log(event.target.value);
-    setUsernameError("");
-    setUsername(event.target.value);
+  interface FormData {
+    username: string;
+    password: string;
+  }
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>();
+  const onSubmitValid = (data: any) => {
+    // console.log("valid", data);
   };
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    console.log(username);
-    if (username === "") {
-      setUsernameError("Not empty pls.");
-    }
-    if (username.length < 10) {
-      setUsernameError("too short");
-    }
+  const onSubmitInValid = (data: any) => {
+    // console.log("Invalid", data);
   };
+  console.log(errors);
   return (
     <AuthLayout>
       <PageTitle title="Login" />
@@ -47,20 +48,31 @@ function Login() {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <form onSubmit={handleSubmit}>
-          {usernameError}
+        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInValid)}>
+          {/* useForm이 어떤 Input을 사용하는지 알아야하기 때문에 항상 input에 name값을 주어야함 */}
           <Input
-            onChange={onUsernameChange}
-            value={username}
+            {...register("username", {
+              required: "Username is required.",
+              minLength: {
+                value: 5,
+                message: "Username should be longer than 5 chars.",
+              },
+              pattern: {
+                message:
+                  "한글, 특수문자를 제외한 1~15자 이내 영문만 사용 가능합니다.",
+                value: /^[a-z0-9]{1,15}$/g,
+              },
+              validate: (currentValue) => currentValue.includes("potato"),
+            })}
             type="text"
             placeholder="Username"
           />
-          <Input type="password" placeholder="Password" />
-          <Button
-            type="submit"
-            value="Log in"
-            disabled={username === "" && username.length < 10}
+          <Input
+            {...register("password", { required: "Password is required." })}
+            type="password"
+            placeholder="Password"
           />
+          <Button type="submit" value="Log in" />
         </form>
         <Seperator />
         <FacebookLogin>
